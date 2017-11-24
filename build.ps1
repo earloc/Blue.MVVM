a4baa822684ad15f40b30039303e8e12ea912954
Alexander Clare <a.clare@aw-systems.net>
2017-11-24 11:19:21 +0100

    prepare automatic versioning

---
+++ build.ps1
@@ -0,0 +1,25 @@
+param (
+    [Parameter(Mandatory=$true)]
+    [int]$buildNumber,
+    [string]$packageSuffix
+)
+
+$projects = "Blue.MVVM","Blue.MVVM.Commands", "Blue.MVVM.IoC", "Blue.MVVM.Navigation"
+
+foreach ($project in $projects) {
+
+    $projectFile=".\src/$project/$project.csproj"
+
+    [xml]$xml = Get-Content $projectFile
+    $version = Select-Xml "//Version" $xml | ForEach-Object {$_.Node.'#text'}
+
+    $patchedVersion = "$version.$buildNumber"
+    $packageVersion = "$patchedVersion-$packageSuffix"
+
+    Write-Host "Building $project with version $patchedVersion"
+    dotnet build $projectFile /p:Version=$patchedVersion
+
+    Write-Host "packing $project with package version $packageVersion"
+    dotnet pack  $projectFile /p:Version=$packageVersion
+
+}
--- publish-local.bat
+++
@@ -1,1 +0,0 @@
-dotnet publish src -o c:\working\packages
